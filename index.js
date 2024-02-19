b5a494bdb3513a98dcfa0c982dd5c1f56c14d685
Ibijolaade <134616411+Ibijolaade@users.noreply.github.com>
2024-02-19 17:07:25 +0100

    all files downloaded

---
+++ index.js
@@ -0,0 +1,15 @@
+let coountEl = document.getElementById("count-el")
+let saveEl = document.getAnimations("save-el")
+count = 0
+
+function increment() {
+    count = count + 1
+    coountEl.textContent = count
+}
+
+function save() {
+    let regStu = count += 1
+    saveEl.textContent = regStu
+    coountEl.textContent = 0
+    count = 0
+}
